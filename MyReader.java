823e451f8dbeeffa899d30cb64bec84099a08498
IvanBykov <volk@sugardas.lt>
2017-01-13 17:55:47 +0200

    MyReader

---
+++ MyReader.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class MyReader
+{
+	Scanner sc;
+	public MyReader;
+	{
+		sc = new Scanner(System.in);
+	}
+	public int read()
+	{
+		return sc.nextInt();
+	}
+}
